bbe57b0b758955e0eeefebfaeb566e9af9c97202
David Ackerman <dackerman@stainlessapi.com>
2024-12-06 15:45:31 -0500

    added migration

--- internal/services/product_variant/migrations.go
+++ internal/services/product_variant/migrations.go
@@ -12,8 +12,8 @@ import (
 var _ resource.ResourceWithUpgradeState = (*ProductVariantResource)(nil)
 
 var conversions = migration.RegisterMigration("stlstore_product_variant", map[string]migration.SchemaConversion{
-	// "price":      migration.SkipConversion{},
-	// "addl_price": migration.RenameProperty{NewName: "price"},
+	"price":      migration.SkipConversion{},
+	"addl_price": migration.RenameProperty{NewName: "price"},
 })
 
 func (r *ProductVariantResource) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
